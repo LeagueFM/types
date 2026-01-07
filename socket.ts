e37bacb9d07a589039e3009c923c338af4cdee1d
Oscar <56079903+OscarNOW@users.noreply.github.com>
2026-01-07 13:55:21 +0100

    created socket types

--- socket.ts
+++ socket.ts
@@ -1,11 +1,25 @@
 import type { Server, Socket } from 'socket.io';
+import type { parsedNowPlayingInfo, Show } from './index';
 
 export interface ServerToClientEvents {
-    connect: () => void;
+    connect(): void;
+
+    nowPlayingV2(data: parsedNowPlayingInfo): void;
+    songRequestsOpen(data: boolean): void;
+    clockedInV2(data: Show): void;
+
+    scheduleUpdate(data: null): void;
+    bannersUpdate(data: null): void;
+    blogUpdate(data: null): void;
+    top20Update(data: null): void;
+    vacaturesUpdate(data: null): void;
 }
 
 export interface ClientToServerEvents {
-    connect: () => void;
+    connect(): void;
+
+    ready(): void;
+    enableLyrics(): void;
 }
 
 export interface InterServerEvents {
