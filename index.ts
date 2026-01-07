42b4f117f88d85dafdf11e31fc5cdc88f52be7a4
Oscar <56079903+OscarNOW@users.noreply.github.com>
2026-01-07 14:04:50 +0100

    improved types

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import type { Server, Socket } from 'socket.io';
+
 export type Show = {
   id: string;
   start: string;
@@ -113,3 +115,35 @@ export type Vacature = {
   voiceRequired: boolean;
   file: string;
 };
+
+export interface ServerToClientEvents {
+  connect(): void;
+
+  nowPlayingV2(data: parsedNowPlayingInfo): void;
+  songRequestsOpen(data: boolean): void;
+  clockedInV2(data: Show): void;
+
+  scheduleUpdate(data: null): void;
+  bannersUpdate(data: null): void;
+  blogUpdate(data: null): void;
+  top20Update(data: null): void;
+  vacaturesUpdate(data: null): void;
+}
+
+export interface ClientToServerEvents {
+  connect(): void;
+
+  ready(): void;
+  enableLyrics(): void;
+}
+
+export interface InterServerEvents {
+  // 
+}
+
+export interface SocketData {
+  // 
+}
+
+export type ioType = Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>;
+export type socketType = Socket<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>;
